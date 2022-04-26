157403e2669cfa7d61d439b1b01fc53c212a5070
Tyler Sehon <tsehon@usc.edu>
2022-04-26 16:38:26 -0700

    yuh

--- src/main/java/DAO.java
+++ src/main/java/DAO.java
@@ -7,27 +7,59 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DAO {
-	
-	public static int registerUser (User user) throws ClassNotFoundException {
-		String sqlString = "INSERT INTO users " + 
-				"(user_id, user_name, user_img_url, user_email) VALUES " + "(?, ?, ?, ?)";
-		int result = 0;
+    private static Connection conn;
+
+    DAO () {
+        conn = null;
+    }
+
+    public static void openConnection() throws SQLException {
+        conn = JDBCUtil.getConnection();
+    }
+
+    public static void closeConnection() throws SQLException {
+        conn.close();
+    }
+
+	public static int addUser(User user) throws SQLException {
+        openConnection();
 		
-		try (Connection connection = JDBCUtil.getConnection()) {
-			PreparedStatement statement = connection.prepareStatement(sqlString);
+		String sqlString = "INSERT INTO users (user_id, user_name, user_img_url, user_email) VALUES (?, ?, ?, ?)";
+		int result = 0;
+
+        try {
+			PreparedStatement statement = conn.prepareStatement(sqlString);
 			statement.setString(1,  user.getId());
 			statement.setString(2,  user.getName());
 			statement.setString(3,  user.getImgUrl());
 			statement.setString(4,  user.getEmail());
 			
 			result = statement.executeUpdate();
-			
-			connection.close();
 		} catch (SQLException e) {
             e.printStackTrace();
 		} 
-		
+
+        closeConnection();
 		return result;
 	}
-	
+
+    public static int addTicket(Ticket ticket) throws SQLException {
+        openConnection();
+		
+		String sqlString = "INSERT INTO tickets (departure, location) VALUES (?, ?)";
+		int result = 0;
+
+        try {
+			PreparedStatement statement = conn.prepareStatement(sqlString);
+			statement.setString(1,  "departure");
+			statement.setString(2,  ticket.getLocation());
+			
+			result = statement.executeUpdate();
+		} catch (SQLException e) {
+            e.printStackTrace();
+		} 
+
+        closeConnection();
+        return result;
+    }
 }
