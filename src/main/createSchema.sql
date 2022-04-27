4b676ea41d800e58b15fb17aebcbbc14b4a8d053
Max Albright <mdalbrig@usc.edu>
2022-04-26 21:38:41 -0700

    adjusted schema (email is primary key now)

--- src/main/createSchema.sql
+++ src/main/createSchema.sql
@@ -11,14 +11,13 @@ CREATE TABLE `tickets` (
   PRIMARY KEY (`ticketID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `users` (
-  `userID` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`userID`),
+  PRIMARY KEY (`email`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `userticketbridge` (
-  `userID` int DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
   `rideID` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
