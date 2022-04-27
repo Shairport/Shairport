0666892be82e3c20f912201b8aa410e75240aaf6
Max Albright <mdalbrig@usc.edu>
2022-04-26 19:29:18 -0700

    added schema creation query

---
+++ src/main/createSchema.sql
@@ -0,0 +1,23 @@
+DROP Database if exists Shairport;
+CREATE DATABASE `Shairport`;
+USE Shairport;
+CREATE TABLE `tickets` (
+  `ticketID` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(45) NOT NULL,
+  `airport` varchar(45) NOT NULL,
+  `pickuptime` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  PRIMARY KEY (`ticketID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `userticketbridge` (
+  `userID` int DEFAULT NULL,
+  `rideID` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
