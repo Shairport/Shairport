3009ed24f5268fbfe673bc0a46197815c682456b
Max Albright <mdalbrig@usc.edu>
2022-04-27 10:59:08 -0700

    minor fix to better access packages

--- src/main/java/ticketservlet.java
+++ src/main/java/ticketservlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import main.java.util.JDBCUtil;
+import util.JDBCUtil;
 
 
 /**
