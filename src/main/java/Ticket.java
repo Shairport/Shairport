34dc260799866263639c115bfe8de15ad4f8bc56
Max Albright <mdalbrig@usc.edu>
2022-04-27 14:55:31 -0700

    added TicketParser which returns sorted tickets

--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,42 +1,85 @@
-package main.java;
-
-import java.time.LocalDateTime;
-import java.util.Set;
+import java.time.*;
 
 public class Ticket {
-    private Set<User> users;
-    private LocalDateTime pickupTime;
-    private String location;
+	private int ticketID;
+    private String pickupdate; // yyyy-mm-dd
+    private String airport;
+    private String pickuptime; // when they want to be at the airport
+    private String location; // pickup location
+    private String phonenumber;
+    private Integer sortingtime;
 
-    public Set<User> getUsers() {
-        return users;
+    Ticket(int t, String pd, String a, String pt, String l, String p) {
+    	ticketID = t;
+    	pickupdate = pd;
+    	airport = a;
+    	pickuptime = pt;
+    	location = l;
+    	phonenumber = p;
+
     }
 
+    public int getTicketID() {
+        return ticketID;
+    }
+    public String getPickupdate() {
+        return pickupdate;
+    }
+    public String getAirport() {
+        return airport;
+    }
+    public Integer getPickuptime() {
+    	boolean isAM = true;
+    	String currpickuptime = pickuptime;
+    	if (pickuptime.contains("PM")) {
+    		isAM = false;
+    		currpickuptime = pickuptime.substring(0, 5);
+    	} else if (pickuptime.contains("AM")) {
+    		currpickuptime = pickuptime.substring(0, 5);
+    	}
+    	String [] hourMin = currpickuptime.split(":");
+    	int hour = Integer.parseInt(hourMin[0]);
+    	if (!isAM) { hour += 12;}
+    	int minute = Integer.parseInt(hourMin[1]);
+    	return (hour * 60 + minute);
+        
+    }
     public String getLocation() {
         return location;
     }
-
-    public LocalDateTime getPickupTime() {
-        return pickupTime;
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+    public Integer getSortingtime() {
+    	return sortingtime;
+    }
+    public void setSortingtime(int x) {
+    	sortingtime = x;
     }
 
-    public void setUsers(Set<User> users) {
-        this.users = users;
+ /*
+    public void setAirport(String airport) {
+        this.airport = airport;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public void setFlightDate(LocalDate flightDate) {
+        this.flightDate = flightDate;
     }
 
-    public void setPickupTime(LocalDateTime pickupTime) {
-        this.pickupTime = pickupTime;
+    public void setFlightTime(LocalTime flightTime) {
+        this.flightTime = flightTime;
     }
 
-    public void addUser(User user) {
-        users.add(user);
+    public void setArriveBy(LocalTime arriveBy) {
+        this.arriveBy = arriveBy;
     }
 
-    public void removeUser(User user) {
-        users.remove(user);
+    public void setStartTimeRange(LocalDateTime startTimeRange) {
+        this.startTimeRange = startTimeRange;
     }
+   
+    public void setEndTimeRange(LocalDateTime endTimeRange) {
+        this.endTimeRange = endTimeRange;
+    } 
+    */
 }
